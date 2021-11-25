a0cdc7ccd62a10f707ae939cac25bf1d532f6975
cesperon <cesperon4@gmail.com>
2021-11-25 06:42:46 -0800

    implemented basic ERC721 standards

--- src/contracts/ERC721Connector.sol
+++ src/contracts/ERC721Connector.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.10;
 
 import "./ERC721Metadata.sol";
+import "./ERC721.sol";
 
-//inhertiance keywords all functions in ERC721Metada that are public are now in the connector
-contract ERC721Connector is ERC721Metadata {
+//inhertiance keywords all functions in ERC721Metada, ERC721 that are public are now in the connector
+contract ERC721Connector is ERC721Metadata, ERC721 {
   constructor(
     string memory name,
     string memory symbol,
